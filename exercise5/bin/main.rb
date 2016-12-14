3451249b7b8dda46071e4c476635b3454a2a8992
obi ubaka <ubaka@dealdey.com>
2016-12-14 15:07:04 +0100

    added more ways of creating singleton methods to exercise 5

--- exercise5/bin/main.rb
+++ exercise5/bin/main.rb
@@ -1,19 +1,41 @@
 string = "Hello World"
 
+# 1
 def string.say_hello
   puts "#{self}"
 end
 
+# 2
 class << string
   def content
     puts "content: #{self}"
   end
 end
 
+# 3
 string.define_singleton_method(:message) do
   puts "message: #{self}"
 end
 
-string.say_hello
-string.content
-string.message
+# 4
+class String
+  class << self
+    def single
+      puts "single: #{self}"
+    end
+  end
+end
+
+# 5
+module TestModule
+  def hello
+    puts 'hello from module'
+  end
+end
+
+class String
+  extend TestModule
+end
+
+p string.singleton_methods
+p String.singleton_methods
